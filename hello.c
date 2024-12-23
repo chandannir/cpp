bf4302f7057a1c6f03369536c17d208ed1b5f06d
chandannir <chandannir82@gmail.com>
2024-12-23 14:37:06 -0500

    changed

--- hello.c
+++ hello.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <cs50.h>
+
 
 int main(void)
 {
-    string answer = get_string("Whats your name? ");
-    printf("hello, %s\n", answer);
+    printf("hello, world\n");
 }
