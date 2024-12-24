036669495d11d3821c5d090ec3dc0c7e53a1805a
chandannir <chandannir82@gmail.com>
2024-12-23 23:30:32 -0500

    fixed variable

--- conditionals.c
+++ conditionals.c
@@ -8,9 +8,9 @@ int main(void)
 
     if (x < y)
     {
-        printf("x is less than y\n");
+        printf("x is less than y\n ");
     }
     else{
-        printf("x is greater than y/n");
+        printf("x is greater than y\n ");
     }
 }
