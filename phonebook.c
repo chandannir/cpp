2617b0c7ee34f2caa9c94bff3d90e8262df1e6e5
chandannir <chandannir82@gmail.com>
2024-12-26 00:52:12 -0500

    lecture 3

--- phonebook.c
+++ phonebook.c
@@ -2,18 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 
+ typedef struct 
+    {
+        string name;
+        string number;
+    }
+    person;
+
+
 int main(void)
-{
-    string names[] = {"Chandan", "Manjender"};
-    string numbers[] = {"613-869-9124", "613-600-0329"};
+{   
+    person people[2];
+
+    people[0].name = "Chandan";
+    people[0].number = "1";
+
+    people[1].name = "Manjender";
+    people[1].number = "2";
 
     string name = get_string("Name: ");
-
     for(int i = 0; i < 2; i++)
     {
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name , name) == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
