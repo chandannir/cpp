c2b70217d01cdf79ea8ab1f42fb934e2f97de50a
chandannir <chandannir82@gmail.com>
2024-12-28 22:33:47 -0500

    memory

--- copy.c
+++ copy.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (void)
 {
     char *s = get_string("s: ");
+    if(s == NULL)
+    {
+        return 1;
+    }
 
-    char *t = s;
+    char *t = malloc(strlen(s) + 1);
+    if(t == NULL)
+    {
+        return 1; 
+    }
 
+    strcpy(t, s);
 
-    if (strlen(t) > 0)
+    if(strlen(t) > 0)
     {
         t[0] = toupper(t[0]);
     }
@@ -18,4 +28,8 @@ int main (void)
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    free(t);
+
+    return 0;
 }
